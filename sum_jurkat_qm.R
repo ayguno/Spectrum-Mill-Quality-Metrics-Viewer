8ef310448ef250061719c3e9df0f123cbef20e9d
Ozan Aygun <oaygun@mit.edu>
2016-08-04 16:59:11 -0400

    Fixed multiple bugs fixed and input modified
    1. Fixed a bug that result from users opening ssv files in excel and saving them as tab-delimited files
    2. Fixed a bug that result from user-dependent corruption of the input files. When a column name is corrupted from the original, an error message is generated
    3. Modified the input of the function to the directory (rather than file). The function can be called from any directory, but the directory that needs to be analyzed should be entered as input. Function will set the directory back upon completion.

--- sum_jurkat_qm.R
+++ sum_jurkat_qm.R
@@ -1,11 +1,21 @@
 
 #This function gets the quality metrics file as an input and reports the relevant values 
 
-sum_jurkat_qm<-function(qm){
+#directory = provide the directory of the QM (chr)
+
+# Fixed the bugs related to different file types
+
+sum_jurkat_qm<-function(directory){
   
-  QM<-read.csv2(qm,header=TRUE)
+  old_dir<-getwd()
+  setwd(directory)
   
-  Di<-QM$File[1]
+  QM1<-read.csv2("qualityMetricsExport.1.ssv", header = T)
+  QM2<-read.delim("qualityMetricsExport.1.ssv", header = T)
+  
+  ifelse(ncol(QM1)> ncol(QM2),QM<-QM1, QM<- QM2)
+ 
+  Di<-QM[1,1]
   A<-QM$MS.MS.spectra.collected.C[1]
   B<-QM$MS.MS.spectra.valid.V[1]
   C<-QM$Collection.Yield.V.C....[1]
@@ -17,20 +27,28 @@ sum_jurkat_qm<-function(qm){
   H<-QM$Median.MS2.fill.time.mid.90..matched.spectra..msec.[1]
   I<-QM$Distinct.Peps.CS.Total....[1]
   
-  s<-data.frame(Jurkat=Di,
-                MS.MS.spectra.collected.C=A,
-                MS.MS.spectra.valid.V=B,
-                Collection.Yield.V.C=C,
-                Validation.Yield.V.F=D,
-                Mean.Precursor.Mass.Error..ppm=E,
-                Time.span.mid.90..matched.spectra.in.run..min=X,
-                Median.MS1.peak.width.mid.90..matched.spectra..sec=Y,
-                Gradient.Shape.mid.90..matched.spectra.in.run=G,
-                Median.MS2.fill.time.mid.90..matched.spectra..msec=H,
-                Distinct.Peps.CS.Total=I)
+  test<- list(Di,A,B,C,D,E,X,Y,G,H,I) 
+  testNull<-sapply(test,is.null)
   
-  s<-t(s)
+  if(sum(testNull) > 0 ) 
+      {print("Error! Quality metrics file can not be read,check the file and column names")}
+  else
+      {s<-data.frame(Jurkat=Di,
+                    MS.MS.spectra.collected.C=A,
+                    MS.MS.spectra.valid.V=B,
+                    Collection.Yield.V.C=C,
+                    Validation.Yield.V.F=D,
+                    Mean.Precursor.Mass.Error..ppm=E,
+                    Time.span.mid.90..matched.spectra.in.run..min=X,
+                    Median.MS1.peak.width.mid.90..matched.spectra..sec=Y,
+                    Gradient.Shape.mid.90..matched.spectra.in.run=G,
+                    Median.MS2.fill.time.mid.90..matched.spectra..msec=H,
+                    Distinct.Peps.CS.Total=I)
+      
+      s<-t(s)
+      
+      write.csv(file=paste(QM$File[1],"QM_summary.csv",sep="_"),s)}
   
-  write.csv(file=paste(QM$File[1],"QM_summary.csv",sep="_"),s)
+  setwd(old_dir)
   
 }
